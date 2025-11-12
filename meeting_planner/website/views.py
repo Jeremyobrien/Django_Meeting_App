fafc2a0343d3e69a20404248751f7dab1d9445c6
Jeremyobrien <jeremy.j.obrien1@gmail.com>
2025-11-12 18:55:01 -0500

    first commit

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner")
+
+def date(request):
+    return HttpResponse("This page was served at" + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("This meeting planner application has scheduling functionality and admin view capabilities.")
