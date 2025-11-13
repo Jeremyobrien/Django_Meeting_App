2e7c121ca1a184a5d2a86da985419a0bbf353b25
Jeremyobrien <jeremy.j.obrien1@gmail.com>
2025-11-13 15:27:35 -0500

    Add templates

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,8 +2,11 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from meetings.models import Meeting
+ 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner")
+    return render(request, "website/welcome.html",
+                {"num_meetings": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was served at" + str(datetime.now()))
