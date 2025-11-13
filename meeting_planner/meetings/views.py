cd7d3a5b44831748d69475da35be297ba2f48eab
Jeremyobrien <jeremy.j.obrien1@gmail.com>
2025-11-13 18:51:35 -0500

    add rooms template, urls.py file to meetings application, added navigation links and styles

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from .models import Meeting
+from .models import Meeting, Room
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+def rooms(request):
+    allrooms = get_list_or_404(Room)
+    return render(request, "meetings/rooms.html", {"rooms":allrooms})
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from meetings.models import Meeting
- 
+from meetings.models import Meeting, Room
+
 def welcome(request):
     return render(request, "website/welcome.html",
-                {"num_meetings": Meeting.objects.count()})
+                {"meetings": Meeting.objects.all()})
 
 def date(request):
     return HttpResponse("This page was served at" + str(datetime.now()))
